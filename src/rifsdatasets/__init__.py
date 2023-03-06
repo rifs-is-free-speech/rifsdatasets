ce85a29650420a075129fc89bc1f47aa710d3598
Emil Joensen <emiljoesen@hotmail.com>
2023-03-06 10:23:29 +0100

    Add dummydataset

--- src/rifsdatasets/__init__.py
+++ src/rifsdatasets/__init__.py
@@ -6,6 +6,6 @@ from rifsdatasets.librivox import LibriVoxDansk
 
 __version__ = "0.0.2"
 
-all_datasets = {"LibriVoxDansk": LibriVoxDansk}
+all_datasets = {"LibriVoxDansk": LibriVoxDansk, "DummyDataset": None}
 
 __all__ = ["all_datasets"]
