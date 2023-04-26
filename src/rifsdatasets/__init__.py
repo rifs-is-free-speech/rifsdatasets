348a4f08b8af2f890fe49e1eb529d8ae7d2854cd
Emil Joensen <emiljoesen@hotmail.com>
2023-04-26 11:25:50 +0200

    Add merge function

--- src/rifsdatasets/__init__.py
+++ src/rifsdatasets/__init__.py
@@ -4,9 +4,10 @@ Package for downloading and loading the rifs datasets
 
 from rifsdatasets.librivox import LibriVoxDansk
 from rifsdatasets.den2radio import Den2Radio
+from rifsdatasets.merge_rifsdatasets import merge_rifsdatasets
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 all_datasets = {"LibriVoxDansk": LibriVoxDansk, "Den2Radio": Den2Radio}
 
-__all__ = ["all_datasets"]
+__all__ = ["all_datasets", "merge_rifsdatasets"]
