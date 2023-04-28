28e8e2b6fc62c7178e992cbc2cfa84a08579ad84
Anders Stendevad <as@flowtale.ai>
2023-04-28 14:01:47 +0200

    Added Forskerzonen

--- src/rifsdatasets/__init__.py
+++ src/rifsdatasets/__init__.py
@@ -4,9 +4,14 @@ Package for downloading and loading the rifs datasets
 
 from rifsdatasets.librivox import LibriVoxDansk
 from rifsdatasets.den2radio import Den2Radio
+from rifsdatasets.forskerzonen import Forskerzonen
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
-all_datasets = {"LibriVoxDansk": LibriVoxDansk, "Den2Radio": Den2Radio}
+all_datasets = {
+    "LibriVoxDansk": LibriVoxDansk,
+    "Den2Radio": Den2Radio,
+    "Forskerzonen": Forskerzonen,
+}
 
 __all__ = ["all_datasets"]
