02b57467f266e94e2e5cefd4db65a38d55baad91
Emil Joensen <emiljoesen@hotmail.com>
2023-05-08 10:40:06 +0200

    Add commonvoicedansk

--- src/rifsdatasets/__init__.py
+++ src/rifsdatasets/__init__.py
@@ -6,16 +6,18 @@ from rifsdatasets.librivox import LibriVoxDansk
 from rifsdatasets.den2radio import Den2Radio
 from rifsdatasets.forskerzonen import Forskerzonen
 from rifsdatasets.danpass import DanPass
+from rifsdatasets.commonvoicedansk import CommonVoiceDansk
 from rifsdatasets.merge_rifsdatasets import merge_rifsdatasets
 from rifsdatasets.split_dataset import split_dataset
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 all_datasets = {
     "LibriVoxDansk": LibriVoxDansk,
     "Den2Radio": Den2Radio,
     "Forskerzonen": Forskerzonen,
     "DanPASS": DanPass,
+    "CommonVoiceDansk": CommonVoiceDansk,
 }
 
 __all__ = ["all_datasets", "merge_rifsdatasets", "split_dataset"]
--- src/rifsdatasets/commonvoicedansk.py
+++ src/rifsdatasets/commonvoicedansk.py
@@ -5,7 +5,7 @@ This module contains the Danpass dataset.
 from rifsdatasets.base import Base
 
 
-class DanPASS(Base):
+class CommonVoiceDansk(Base):
     """
     Dataset for the DanPASS dataset.
     """
@@ -37,9 +37,9 @@ class DanPASS(Base):
         import os
         from os.path import join
 
-        target = join(target_folder, "DanPASS")
+        target = join(target_folder, "CommonVoiceDansk")
         if verbose and not quiet:
-            print(f"Downloading DanPASS to '{target_folder}'")
+            print(f"Downloading CommonVoiceDansk to '{target_folder}'")
         if os.path.exists(target):
             if verbose and not quiet:
                 print(
@@ -53,7 +53,7 @@ class DanPASS(Base):
             if verbose and not quiet:
                 print("Created temporary directory", tmpdirname)
             Repo.clone_from(
-                url="git@github.com:rifs-is-free-speech/DanPASS.git",
+                url="git@github.com:rifs-is-free-speech/CommonVoiceDansk.git",
                 to_path=tmpdirname,
                 progress=None if quiet else CloneProgress(),
             )
@@ -64,8 +64,11 @@ class DanPASS(Base):
             if verbose and not quiet:
                 print(f"Created folder '{target}'")
             move(join(tmpdirname, "all.csv"), f"{target}/all.csv")
+            move(join(tmpdirname, "train.csv"), f"{target}/all.csv")
+            move(join(tmpdirname, "dev.csv"), f"{target}/all.csv")
+            move(join(tmpdirname, "test.csv"), f"{target}/all.csv")
             if verbose and not quiet:
-                print(f"Moved all.csv to '{target}'")
+                print(f"Moved csv files to '{target}'")
             move(join(tmpdirname, "text"), f"{target}/text")
             if verbose and not quiet:
                 print(f"Moved text/ to '{target}'")
