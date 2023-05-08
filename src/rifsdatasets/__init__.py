d30dd6e6ab11c7751575dd2bd15aab32e817be1e
Anders Stendevad <as@flowtale.ai>
2023-05-08 10:54:14 +0200

    Fixed typo

--- src/rifsdatasets/__init__.py
+++ src/rifsdatasets/__init__.py
@@ -5,7 +5,7 @@ Package for downloading and loading the rifs datasets
 from rifsdatasets.librivox import LibriVoxDansk
 from rifsdatasets.den2radio import Den2Radio
 from rifsdatasets.forskerzonen import Forskerzonen
-from rifsdatasets.danpass import DanPass
+from rifsdatasets.danpass import DanPASS
 from rifsdatasets.nstdanishspråkbanken import NSTDanishSpråkbanken
 from rifsdatasets.commonvoicedansk import CommonVoiceDansk
 from rifsdatasets.merge_rifsdatasets import merge_rifsdatasets
@@ -17,7 +17,7 @@ all_datasets = {
     "LibriVoxDansk": LibriVoxDansk,
     "Den2Radio": Den2Radio,
     "Forskerzonen": Forskerzonen,
-    "DanPASS": DanPass,
+    "DanPASS": DanPASS,
     "NSTDanishSpråkbanken": NSTDanishSpråkbanken,
     "CommonVoiceDansk": CommonVoiceDansk,
 }
